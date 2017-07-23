1dd1b1b203c4d240c70d10239418d085aea4a8d4
ankanch <kanchisme@gmail.com>
2017-07-23 16:56:16 +0800

    UPDATE

--- Homtworks/hw3/Q5.py
+++ Homtworks/hw3/Q5.py
@@ -14,17 +14,29 @@ aflower = numpy.float32(aflower)
 
 U,S,Vt = linalg.svd(aflower)
 
-pyplot.plot(S,'b.')
-pyplot.show()
+#pyplot.plot(S,'b.')
+#pyplot.show()
 
 ####
-KL = [20,50,100,200]
-for K in KL:
-    Sk = numpy.diag(S[:K])
-    Uk = U[:, :K]
-    Vtk = Vt[:K, :]
-
-    aImk = numpy.dot(Uk, numpy.dot( Sk, Vtk))
-    Imk = Image.fromarray(aImk)
-    Imk.show()
+KL = [0,0,200,100,50,20]
+fig = pyplot.figure()
+pyplot.subplots_adjust(hspace=0.5)
+a = fig.add_subplot(3,2,1)
+a.set_title("singular values")
+pyplot.plot(S,'b.')
+a = fig.add_subplot(3,2,2)
+a.set_title("original image")
+pyplot.imshow(aflower, cmap='gray')
+for i,K in enumerate(KL):
+    if i > 1:
+        Sk = numpy.diag(S[:K])
+        Uk = U[:, :K]
+        Vtk = Vt[:K, :]
+        aImk = numpy.dot(Uk, numpy.dot( Sk, Vtk))
+        Imk = Image.fromarray(aImk)
+        a = fig.add_subplot(3,2,i+1)
+        a.set_title(">>>>> Image with K=%d <<<<<"%K)
+        pyplot.imshow(aImk,  cmap='gray')
+        #Imk.show()
+pyplot.show()
 
