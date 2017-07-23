829c65eead792429f5e455886fe9c957c6edca83
ankanch <kanchisme@gmail.com>
2017-07-23 22:13:19 +0800

    UPDATE CODE

--- Homtworks/hw3/Q4.py
+++ Homtworks/hw3/Q4.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sympy import Matrix
 from numpy import linalg
 
 
@@ -11,3 +12,10 @@ print("result for Q4.c=\n",r)
 b = np.asarray([[2],[2.0001]])
 r = linalg.solve(A,b)
 print("result for Q4.c after accurte b=\n",r)
+
+B = Matrix([[100,100],[100,100.01]])
+print("matrix det=\n",B.det())
+print("matrix condition=\n",B.condition_number())
+
+print("22matrix det=\n",linalg.det(A))
+print("22matrix condition=\n",linalg.cond(A))
