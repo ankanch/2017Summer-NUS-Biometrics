18830c51fe35eff8c62ed80703e0d7bf2607d8e3
ankanch <kanchisme@gmail.com>
2017-07-29 17:33:22 +0800

    Update task 4

--- Homtworks/hw5/facerecog.py
+++ Homtworks/hw5/facerecog.py
@@ -14,42 +14,65 @@ W1 = W[:,:K1]
 print(">>0:PCA: We.shape=",We.shape,"\tLL.shape=",LL.shape,"\tm.shape=",m.shape)
 print(">>0:LDA: W1.shape=",W1.shape)
 proj_pca_train = np.dot(We.T,(faces_train.T-m).T).T
-
-#reduced_faces_lda =  np.dot(W1.T,faces_train.T-m).T
 print(">>1:PCA-Projection: train.shape=",proj_pca_train.shape)
-print(">>1:")
+reduced_faces_lda =  np.dot(W1.T,(faces_train.T-m).T)
+print(">>1:LDA-ReduceDimension: train.shape=",reduced_faces_lda.shape)
+W_lda, Centers, classLabels = FF.myLDA(reduced_faces_lda,idLabel_train[:K1])
+proj_lda_train = np.dot(np.dot(W_lda.T,W1.T),(faces_train.T-m).T)
+print(">>1:LDA-Projection: train.shape=",proj_lda_train.shape)
 # compute the mean z of all his feature vectors
 faces_mean_train = [[] for x in range(10)]
-for label,vector in zip(idLabel_train[:K],proj_pca_train):
+for label,vector in zip(idLabel_train,proj_pca_train):
     faces_mean_train[label].append(vector)
 faces_mean_train = [ np.mean(x,0) for x in faces_mean_train]
 faces_mean_train = np.asarray(faces_mean_train)
 print(">>2:PCA-FacesMean: train.shape=",faces_mean_train.shape)
 # store these vectors as columns in numpy-matrix Z for every person
 Z = faces_mean_train.T
+Z_lda = Centers
 print(">>3:PCA- Z.shape=",Z.shape)
+print(">>3:LDA- Z.shape=",Z_lda.shape)
 
 # ======================identify========================
 faces_test,idLabel_test = FF.read_faces("./test")
 proj_pca_test = np.dot(We.T,(faces_test.T-m).T)     # column for one sample
+proj_lda_test = np.dot(np.dot(W_lda.T,W1.T),(faces_train.T-m).T)
 print(">>4:PCA- Test-Projection: test.shape=",proj_pca_test.shape)
+print(">>4:LDA- Test-Projection: test.shape=",proj_lda_test.shape)
 
+# identify for PCA : compute distence
 recog = []
 for face in proj_pca_test.T:
     dist = [np.linalg.norm( temp - face ) for temp in faces_mean_train]
     recog.append(dist.index(min(dist)))
 
+# identify for LDA : compute distence
+recog_lda = []
+for face in proj_lda_test.T:
+    dist = [np.linalg.norm( temp - face ) for temp in Z_lda.T]
+    recog_lda.append(dist.index(min(dist)))
 # make up confusion matrix
 confusionmatrix = [ 0 for x in range(10)]
 confusionmatrix = np.asarray([ confusionmatrix for x in range(10)])
+confusionmatrix_lda = [ 0 for x in range(10)]
+confusionmatrix_lda = np.asarray([ confusionmatrix_lda for x in range(10)])
 
 correct = 0
 for recg,real in zip(recog,idLabel_test):
     if recg == real:
         correct+=1
     confusionmatrix[real][recg]+=1
-print(confusionmatrix)
+print(">>4->5: PCA: ConfusionMatrix",confusionmatrix)
+
+correct_lda = 0
+for recg,real in zip(recog_lda,idLabel_test):
+    if recg == real:
+        correct_lda+=1
+    confusionmatrix_lda[real][recg]+=1
+print(">>4->5: LDA: ConfusionMatrix",confusionmatrix_lda)
+
 print(">>5:PCA- OverallCorrectRate:",correct/len(recog),",recgnized",correct,"of",len(recog))
+print(">>5:LDA- OverallCorrectRate:",correct_lda/len(recog_lda),",recgnized",correct_lda,"of",len(recog_lda))
 
 # Task 2>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 reshaped_we = np.asarray([ np.asarray(FF.float2uint8(x)).reshape(160,140) for x in We.T[:8]])
@@ -67,4 +90,29 @@ mean_face = mean_face / 8
 a = fig.add_subplot(3,3,9)
 a.set_title("mean face")
 plt.imshow(mean_face,'gray')
-plt.show()
+#plt.show()
+
+# Task 4>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# project center vectors from LDA spaces to PCA space
+print(">>8:LDA: Centers.shape=",Centers.shape,"\tLDAW.shape=",W_lda.shape)
+Cp = np.dot(W_lda,Centers)
+print(">>9:LDA: Cp.shape=",Cp.shape,"\tme.shape=",m.shape)
+M = np.tile(m,10).reshape(22400,10)
+print(">>10:LDA: W1.shape=",W1.shape,"\tCp.shape=",Cp.shape,"\tM.shape=",M.shape)
+Cr = np.dot(W1,Cp) + M
+print(">>11:LDA: Cr.shape=",Cr.shape)
+reshaped_cr = [x.reshape(160,140) for x in Cr.T]
+# plot them
+fig = plt.figure()
+plt.subplots_adjust(hspace=0.5)
+for x,cf in enumerate(reshaped_cr):
+    a = fig.add_subplot(2,5,x+1)
+    a.set_title("Center %d"%(x+1))
+    plt.imshow(cf,'gray')    
+#plt.show()
+
+# Task 5>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+print(">>12:FusionScheme:  ye.shape=",proj_pca_train.shape,"\tyf.shape=",proj_lda_train.shape)
+alpha = 0.5
+y = np.asarray([[alpha*proj_pca_train],[(1-alpha)*proj_lda_train]])
+print(">>13:FusionScheme:  y.shape=",y.shape)
